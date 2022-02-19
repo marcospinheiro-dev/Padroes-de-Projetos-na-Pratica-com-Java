5e237c325ceb0e567a5d0e711522c926f094f20c
marcospinheiro-dev <marcos.pinheiro.dev@gmail.com>
2022-02-19 00:31:25 -0300

    First commit

---
+++ src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "preguiçoso"
+ *
+ *
+ * @autor falvojr
+ */
+
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
